95614a80085357833540efa1704221b91a6b415a
tamirherzberg <tamir.herzberg@gmail.com>
2022-04-04 20:09:29 +0300

    some preprocessing

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -7,6 +7,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
 
 
@@ -23,7 +24,34 @@ def load_data(filename: str):
     Design matrix and response vector (prices) - either as a single
     DataFrame or a Tuple[DataFrame, Series]
     """
-    raise NotImplementedError()
+    # load and delete missing values, duplicates
+    full_data = pd.read_csv(filename).dropna().drop_duplicates()
+    preprocess(full_data)
+
+
+def preprocess(full_data):
+    # delete 33 rooms house, 0 bathrooms houses
+    full_data.drop(columns=['id', 'zipcode', 'date'], inplace=True)
+    full_data.drop(full_data.index[full_data['bedrooms'] == 33], inplace=True)
+    full_data.drop(full_data.index[full_data['bathrooms'] <= 0], inplace=True)
+    full_data.drop(full_data.index[full_data['sqft_living'] > 13000], inplace=True)  # removes a fishy entry
+    # handle yr_renovated
+    update_est_year_built(full_data)
+    # handle categorical data?
+
+
+def update_est_year_built(df: pd.DataFrame):
+    """
+    Estimate year built, taking into consideration if it renovated and if so when.
+    Subtracts a maximum of 50 years from the renovation year, depending on
+    how big the difference between the house building and renovation.
+    The calculation was based on the maximum span between the two years
+    in the dataset, which is 130.
+    """
+    for i, val in enumerate(df['yr_built']):
+        if df['yr_renovated'][i] != 0:
+            df['yr_built'][i] -= ((df['yr_renovated'][i] - val) / 130) * 50
+    df.rename(columns={'yr_built': 'yr_est'})
 
 
 def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") -> NoReturn:
