f4799fcc65789c1ecb79d6873e5ef80c2e183842
tamirherzberg <tamir.herzberg@gmail.com>
2022-04-07 00:27:16 +0300

    ISR temp - doy graph done

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -5,9 +5,11 @@ from IMLearn.utils import split_train_test
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import plotly.io as pio
 
 pio.templates.default = "simple_white"
+pio.renderers.default = "chrome"
 
 FILENAME = "../datasets/City_Temperature.csv"
 
@@ -26,25 +28,27 @@ def load_data(filename: str) -> pd.DataFrame:
     """
     full_data = pd.read_csv(filename, parse_dates=['Date'], dayfirst=True).dropna().drop_duplicates()
     # preprocessing
-    full_data.drop(full_data.index[full_data['Temp'] <= -70])
+    full_data.drop(full_data.index[full_data['Temp'] <= -70], inplace=True)
     full_data['DayOfYear'] = full_data['Date'].dt.day_of_year
     return full_data
-
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of city temperature dataset
-    load_data(FILENAME)
+    df = load_data(FILENAME)
 
     # Question 2 - Exploring data for specific country
-    # raise NotImplementedError()
-    #
-    # # Question 3 - Exploring differences between countries
-    # raise NotImplementedError()
-    #
-    # # Question 4 - Fitting model for different values of `k`
-    # raise NotImplementedError()
-    #
-    # # Question 5 - Evaluating fitted model on different countries
-    # raise NotImplementedError()
+    isr_subset = df[df['Country'] == "Israel"]
+    isr_subset['Year'] = isr_subset['Year'].astype(str)
+    px.scatter(isr_subset, x='DayOfYear', y='Temp', color='Year',
+               title=f"Temperature in Israel As A Function Of Day Of The Year").show()
+
+    # Question 3 - Exploring differences between countries
+    raise NotImplementedError()
+
+    # Question 4 - Fitting model for different values of `k`
+    raise NotImplementedError()
+
+    # Question 5 - Evaluating fitted model on different countries
+    raise NotImplementedError()
