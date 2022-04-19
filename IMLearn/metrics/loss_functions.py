7f5859660eb88c1c5cff92bee5ab3bf7c8c964f9
tamirherzberg <tamir.herzberg@gmail.com>
2022-04-19 11:26:22 +0300

    misclassification_error

--- IMLearn/metrics/loss_functions.py
+++ IMLearn/metrics/loss_functions.py
@@ -36,8 +36,11 @@ def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray, normalize: b
     -------
     Misclassification of given predictions
     """
-    raise NotImplementedError()
-
+    mult = y_true * y_pred
+    if normalize:
+        return np.sum(mult < 0, axis=0) / mult.shape[0]
+    else:
+        np.sum(mult < 0, axis=0)
 
 def accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     """
