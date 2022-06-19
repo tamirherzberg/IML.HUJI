4dbb5c792bd3db56125777c60afe2e308e307a56
tamirherzberg <tamir.herzberg@gmail.com>
2022-06-19 23:58:50 +0300

    get_gd_callback

--- exercises/gradient_descent_investigation.py
+++ exercises/gradient_descent_investigation.py
@@ -73,7 +73,12 @@ def get_gd_state_recorder_callback() -> Tuple[Callable[[], None], List[np.ndarra
     weights: List[np.ndarray]
         Recorded parameters
     """
-    raise NotImplementedError()
+    values = []
+    weights = []
+    def callback(weights, val):
+        values.append(val)
+        weights.append(weights)
+    return callback, values, weights
 
 
 def compare_fixed_learning_rates(init: np.ndarray = np.array([np.sqrt(2), np.e / 3]),
